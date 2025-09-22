d752ae1ccd644ca1369175387b3d41c0cd24eb38
Sayu475 <e1x22039@st.oit.ac.jp>
2025-09-22 15:40:52 +0900

    LPF(仮２)

--- src/qei.hpp
+++ src/qei.hpp
@@ -31,6 +31,14 @@ void qei_setup_x4(pcnt_unit_t pcnt_unit, int gpioA, int gpioB)
   pcnt_unit_config(&pcnt_confA);
   pcnt_unit_config(&pcnt_confB);
 
+  // PCNT ハードフィルタを初期化（メイン側で PCNT_FILTER_VALUE を定義していればそれを使う）
+#ifdef PCNT_FILTER_VALUE
+  pcnt_set_filter_value(pcnt_unit, PCNT_FILTER_VALUE);
+#else
+  pcnt_set_filter_value(pcnt_unit, 100);
+#endif
+  pcnt_filter_enable(pcnt_unit);
+
   pcnt_counter_pause(pcnt_unit);
   pcnt_counter_clear(pcnt_unit);
   pcnt_counter_resume(pcnt_unit);
@@ -54,6 +62,13 @@ void qei_setup_x1(pcnt_unit_t pcnt_unit, int gpioA, int gpioB)
   /* Initialize PCNT unit */
   pcnt_unit_config(&pcnt_confA);
 
+  // PCNT ハードフィルタを初期化（メイン側で PCNT_FILTER_VALUE を定義していればそれを使う）
+#ifdef PCNT_FILTER_VALUE
+  pcnt_set_filter_value(pcnt_unit, PCNT_FILTER_VALUE);
+#else
+  pcnt_set_filter_value(pcnt_unit, 100);
+#endif
+  pcnt_filter_enable(pcnt_unit);
 
   pcnt_counter_pause(pcnt_unit);
   pcnt_counter_clear(pcnt_unit);
